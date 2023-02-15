563d8ce65a1ff4a18a8c99c4e43bfe58c124d2e2
sujan-timalsina <sujan.timalsina@apexcollege.edu.np>
2023-02-15 19:17:01 +0545

    Model and Migration added for Project and Skill

---
+++ app/Models/Skill.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Skill extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'string',
+        'image',
+    ];
+
+    public function projects()
+    {
+        return $this->hasMany(Project::class);
+    }
+}
