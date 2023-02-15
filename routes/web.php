e4957ed522d7f1dd5ad75f1687c2e7f4132c29d5
sujan-timalsina <sujan.timalsina@apexcollege.edu.np>
2023-02-15 20:43:12 +0545

    Skill and Project Resourcefull Controller Created

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,9 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Backend\SkillController;
+use App\Http\Controllers\Backend\ProjectController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -26,6 +17,9 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::resource('/skills', SkillController::class);
+    Route::resource('/projects', ProjectController::class); 
 });
 
 require __DIR__.'/auth.php';
